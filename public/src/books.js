e854e5fa04f47e8050f0840b6aea36cddc273296
Scotty McCarthy <119365979+scottymccarthy@users.noreply.github.com>
2022-12-29 17:58:34 -0700

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,28 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+return accounts.find((author) => author.id === id)}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+return accounts.find((books) => book.id === id}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let borrowed = books.filter((book)=> book.borrows.some((borrow) => 
+borrow.retured === false));
 
-function getBorrowersForBook(book, accounts) {}
+let returned = books.filter((book) => 
+book.borrows.every((borrow) => borrow.returned === true));
+//return array with all books borrowed, and another with books returned
+return [[...borrowed], [...returned]];
+}
+
+function getBorrowersForBook(book, accounts) {
+  let total = [];
+book.borrows.map((borrow) => {
+  let account = accounts.find((account) => account.id === borrow.id);
+  account ["returned"] = borrow.returned;
+  total.push(account);
+});
+return total.slice(0,10);
+}
 
 module.exports = {
   findAuthorById,
